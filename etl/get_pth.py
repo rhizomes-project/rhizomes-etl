44f25b74a30fddc56d4874a3a8298007802f647f
Karl Kaebnick <karl.kaebnick@gmail.com>
2020-10-27 19:39:19 -0400

    Use new mapping for PTH sample metadata

--- etl/get_pth.py
+++ etl/get_pth.py
@@ -2,7 +2,6 @@
 
 
 import requests
-import csv
 import sys
 
 from bs4 import BeautifulSoup
@@ -20,59 +19,109 @@ get_records_path = "/oai/?verb=ListRecords&metadataPrefix=oai_dc&set="
 get_records_url = protocol + domain + get_records_path
 
 
-def add_value(record_data, value):
+field_map = {
+    "title":            "Title",
+    "identifier":       "Resource Identifier",
+    # "datestamp":        "",
+    "setspec":          "",
+    "creator":          "Author/Artist",
+    "subject":          "Subjects (Topic/Keywords)",
+    "description":      "Description",
+    "contributor":      "Author/Artist",
+    "publisher":        "",
+    "date":             "Date",
+    "type":             "Resource Type",
+    "format":           "Digital Format",
+    "source":           "Source",
+    "language":         "Language",
+    "coverage":         "Subjects (geographic)",
+}
 
-	if not value.name:
 
-		return False
+def add_value(data, value):
 
-	text = value.get_text().strip()
-	if not text:
+    if not value.name:
 
-		return False
+        return False
 
-	full_value = record_data.get(value.name, '')
-	if full_value:
-		full_value += ', '
+    text = value.get_text().strip()
+    if not text:
 
-	full_value += text
-	record_data[value.name] = full_value
+        return False
 
-	return True
+    full_value = data.get(value.name, [])
+    full_value.append(text)
+    record_data[value.name] = full_value
+
+    return True
+
+
+def clean_value(value):
+
+    if value.startswith("["):
+        value = value[ 1 : ]
+
+    if value.endswith("]"):
+        value = value[ : -1 ]
+
+    return value
+
+def get_value(value):
+
+    if len(value) == 1:
+
+        return clean_value(value=value[0])
+
+    result = []
+    for tmp in value:
+
+        result.append(clean_value(value=tmp))
+
+    return result
+
+
+def print_record(data):
+
+    for name, description in field_map.items():
+
+        if not description:
+
+            continue
+
+        value = data.get(name)
+        if value:
+
+            print(f"{description}: {get_value(value=value)}")
 
 
 if __name__ == "__main__":
 
-	for collection in collections:
+    for collection in collections:
 
-		response = requests.get(f"{get_records_url}{collection}")
+        response = requests.get(f"{get_records_url}{collection}")
 
-		# out = open("data.xml", "w+")
-		# out.write(response.text)
+        # out = open("data.xml", "w+")
+        # out.write(response.text)
 
 
-		fieldnames = [ 'identifier', 'datestamp', 'setspec', 'dc:title', 'dc:creator', 'dc:subject', 'dc:description', 'dc:contributor', 'dc:publisher', 'dc:date', 'dc:type', 'dc:format', 'dc:identifier', 'dc:source', 'dc:language', 'dc:coverage' ]
+        data = BeautifulSoup(response.text, features="lxml-xml")
+        records = data.findAll("record")
 
-		data = BeautifulSoup(response.text, "lxml")
-		records = data.findAll("record")
+        for record in records:
 
-		writer = csv.DictWriter(sys.stdout, fieldnames=fieldnames, delimiter='|')
-		writer.writeheader()
+            print("")
+            record_data = {}
 
-		for record in records:
+            for value in record.header:
 
-			record_data = {}
+                add_value(record_data, value)
 
-			for value in record.header:
+            for value in record.metadata:
 
-				add_value(record_data, value)
+                if value.name == 'dc':
 
-			for value in record.metadata:
+                    for child in value.children:
 
-				if value.name == 'oai_dc:dc':
+                        add_value(record_data, child)
 
-					for child in value.children:
-
-						add_value(record_data, child)
-
-			writer.writerow(record_data)
+            print_record(data=record_data)
