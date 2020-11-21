243e29660bd26637ece75535ab7af547120ef6fa
Karl Kaebnick <karl.kaebnick@gmail.com>
2020-11-21 16:21:22 -0500

    etl: add csv format

--- etl/tools.py
+++ etl/tools.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
+import csv
 import json
+import sys
 
 
 import pdb
@@ -39,7 +41,17 @@ def get_value(value, format="json"):
 
                 else:
 
-                    buf += "\n\t" + tmp
+                    if format == "csv":
+
+                        buf += tmp + "\n"
+
+                    else:
+
+                        buf += "\n\t" + tmp
+
+            if format == "csv":
+
+                buf = buf.strip()
 
             return buf
 
@@ -65,19 +77,38 @@ class MetadataWriter():
 
             self.output = []
 
+        elif self.format == "csv":
+
+            # REVIEW: TODO put these fieldnames in one place only
+            fieldnames = [ "Title", "Author/Artist", "Description", "Date", "Digital Format", "Resource Identifier", "Source", "Subjects (Topic/Keywords)", ]
+
+            self.output = csv.DictWriter(sys.stdout, fieldnames=fieldnames, dialect=csv.QUOTE_ALL)
+
+            self.row_buf = {}
+
         else:
 
             self.output = ""
 
     def start_collection(self):
 
-        pass
+        if self.format == "csv":
+
+            self.output.writeheader()
+
+        else:
+
+            pass
 
     def start_record(self):
 
         if self.format == "json":
 
             self.output.append({})
+
+        if self.format == "csv":
+
+            self.row_buf = {}
 
         else:
 
@@ -91,19 +122,33 @@ class MetadataWriter():
             pos = len(self.output) - 1
             self.output[pos][name] = value
 
+        elif self.format == "csv":
+
+            self.row_buf[name] = value
+
         else:
 
             self.output += f"{name}: {value}\n"
 
     def end_record(self):
 
-        pass
+        if self.format == "csv":
+
+            self.output.writerow(self.row_buf)
+
+        else:
+
+            pass
 
     def end_collection(self):
 
         if self.format == "json":
 
             buf = json.dumps(self.output)
+
+        elif self.format == "csv":
+
+            buf = ""
 
         else:
 
