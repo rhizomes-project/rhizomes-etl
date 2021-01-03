0bc011ae70758aaa4c245c7ddf364d8c291ce506
Karl Kaebnick <karl.kaebnick@gmail.com>
2021-01-03 14:51:24 -0500

    etl: use pagination to retrieve dpla results, add lots of handling for special cases and errors

--- etl/dpla_etl.py
+++ etl/dpla_etl.py
@@ -3,11 +3,11 @@
 
 import requests
 import json
+import sys
 
 from etl_process import BaseETLProcess
 from setup import ETLEnv
 from tools import RhizomeField
-
 
 
 protocol = "https://"
@@ -20,8 +20,11 @@ list_collections_path = "/v2/collections"
 list_collections_url = protocol + domain + list_collections_path + "?api_key=" + api_key
 
 list_items_path = "/v2/items"
-list_items_url = protocol + domain + list_items_path + "?page_size=25&api_key=" + api_key
+list_items_url = protocol + domain + list_items_path + "?page=1&page_size=500&api_key=" + api_key
 
+# dpla_terms = [ "id", "@context", "aggregatedCHO", "dataProvider", "ingestDate", "ingestType", "isShownAt", "object" ]
+dpla_terms = [ "id", "dataProvider", "isShownAt", "object" ]
+original_data_terms = [ "title", "description", "creator", "contributor", "date", "subject", "language", "reference_image_dimensions", "collection_name", "type", "format", "spatial", "coverage" ]
 
 field_map = {
 # REVIEW: Deal with format Dimensions
@@ -51,40 +54,70 @@ class DPLAETLProcess(BaseETLProcess):
 
     def extract(self):
 
-        # dpla_terms = [ "id", "@context", "aggregatedCHO", "dataProvider", "ingestDate", "ingestType", "isShownAt", "object" ]
-        dpla_terms = [ "id", "dataProvider", "isShownAt", "object" ]
-        original_data_terms = [ "title", "description", "creator", "contributor", "date", "subject", "language", "reference_image_dimensions", "collection_name", "type", "format", "spatial", "coverage" ]
-
         data = []
 
-        search_terms = [ "chicano" ]
+        search_terms = [ "chicano", "mexican-american" ]
         for search_term in search_terms:
 
-            response = requests.get(list_items_url + "&q=" + search_term)
+            # For details on pagination, see https://pro.dp.la/developers/requests#pagination
+            count = 1
+            start = 0
+            page = 1
 
-            for doc in response.json()["docs"]:
+            while count > start and page <= 100:
 
-                record = {}
+                response = requests.get(f"{list_items_url}&page={page}&q={search_term}")
 
-                for term in dpla_terms:
+                count = response.json()["count"]
+                start = response.json()["start"]
 
-                    record[term] = doc[term]
+                print(f"search term: {search_term}, page: {page}, total docs: {count}, start: {start}, curr docs: {len(data)}", file=sys.stderr)
 
-                originalRecordString = doc["originalRecord"]["stringValue"]
-                if originalRecordString.startswith('<'):
+                page += 1
 
-                    # REVIEW TODO add support for OAIPMH
-                    continue
+                for doc in response.json()["docs"]:
+
+                    record = {}
+
+                    for term in dpla_terms:
+
+                        if doc.get(term):
+
+                            record[term] = doc[term]
+
+                    # Some records that are part of contributor collections have most of their metadata embedded in the sourceResource string.
+                    if not record.get("title"):
+
+                        for val in [ "title", "description" ]:
+
+                            record[val] = doc['sourceResource'].get(val)
+
+                    originalRecordString = doc["originalRecord"]
+                    if type(originalRecordString) is str:
+
+                        # REVIEW TODO handle situation where metadata is in a URL somwewhere. altho sometimes that url 
+                        # may be unavailable (due to DNS error?)
+
+                        continue
+
+                    else:
+
+                        originalRecordString = originalRecordString.get("stringValue")
+
+                    if not originalRecordString or originalRecordString.startswith('<'):
+
+                        # REVIEW TODO add support for OAIPMH
+                        continue
 
 
-                original_data = json.loads(originalRecordString)
-                for term in original_data_terms:
+                    original_data = json.loads(originalRecordString)
+                    for term in original_data_terms:
 
-                    if term in original_data:
+                        if term in original_data:
 
-                        record[term] = original_data[term]
+                            record[term] = original_data[term]
 
-                data.append(record)
+                    data.append(record)
 
         return data
 
