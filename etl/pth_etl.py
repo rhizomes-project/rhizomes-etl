7369c217b0befe4929e850f1e5fea97acd2a41f9
Karl Kaebnick <karl.kaebnick@gmail.com>
2021-01-04 19:10:48 -0500

    etl: get pth etl script ready for testing and clean up transforms

--- etl/pth_etl.py
+++ etl/pth_etl.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-
+import os
 import re
 import requests
 import sys
@@ -12,14 +12,23 @@ from etl.tools import RhizomeField
 from bs4 import BeautifulSoup
 
 
+running_tests = os.environ.get("RUNNING_UNITTESTS")
+
+
 protocol = "https://"
 domain = "texashistory.unt.edu"
 
 list_sets_path = "/oai/?verb=ListSets"
 list_sets_url = protocol + domain + list_sets_path
 
+RECORD_LIMIT = None
 
 partners = [ "partner:MAMU", "partner:UNT", "partner:UNTA", "partner:UNTGD", ]
+
+if running_tests:
+
+    partners = [ "partner:MAMU" ]
+    RECORD_LIMIT = 1
 
 # REVIEW: TODO Pull in all desired PTH partners
 # REVIEW: TODO revisit ways of further filtering PTH metadata
@@ -31,7 +40,6 @@ resume_records_url = protocol + domain + records_path
 
 
 field_map = {
-# REVIEW: Rework this with RHizomeFields
     "identifier":                             RhizomeField.ID,
     "title":                                  RhizomeField.TITLE,
     "creator":                                RhizomeField.AUTHOR_ARTIST,
@@ -39,14 +47,14 @@ field_map = {
     "description":                            RhizomeField.DESCRIPTION,
     "date":                                   RhizomeField.DATE,
     "type":                                   RhizomeField.RESOURCE_TYPE,
-    "format":                                 RhizomeField.DIGITAL_FORMAT, # dimeionsion info removed by transform()
-    # RhizomeField.DIMENSIONS:                  RhizomeField.DIGITAL_FORMAT, # Added by transform()
-    # RhizomeField.URL:                         RhizomeField.URL, #Added by transform()
+    "format":                                 RhizomeField.DIGITAL_FORMAT,
+    "dimensions":                             RhizomeField.DIMENSIONS,
+    "url":                                    RhizomeField.URL,
     "source":                                 RhizomeField.SOURCE,
     "language":                               RhizomeField.LANGUAGE,
-    # RhizomeField.SUBJECTS_HISTORICAL_ERA:     RhizomeField.SUBJECTS_HISTORICAL_ERA, # Added by transform()
+    "subjects_hist":                          RhizomeField.SUBJECTS_HISTORICAL_ERA,
     "subject":                                RhizomeField.SUBJECTS_TOPIC_KEYWORDS,
-    # RhizomeField.SUBJECTS_GEOGRAPHIC:         RhizomeField.SUBJECTS_GEOGRAPHIC, # Added by transform()
+    "subjects_geo":                           RhizomeField.SUBJECTS_GEOGRAPHIC,
 }
 
 KEYWORDS = [
@@ -56,8 +64,6 @@ KEYWORDS = [
 
 # REVIEW TODO Get canonical list of PTH formats.
 KNOWN_FORMATS = ('image', 'text')
-
-RECORD_LIMIT = 2
 
 
 def has_number(value):
@@ -201,9 +207,8 @@ class PTHETLProcess(BaseETLProcess):
 
                         new_dimensions.append(format)
 
-                del record['format']
-                record[RhizomeField.DIGITAL_FORMAT.value] = new_formats
-                record[RhizomeField.DIMENSIONS.value] = new_dimensions
+                record["format"] = new_formats
+                record["dimensions"] = new_dimensions
 
             # Add in a URL value.
             identifiers = record["identifier"]
@@ -220,36 +225,34 @@ class PTHETLProcess(BaseETLProcess):
 
                     new_ids.append(identifier)
 
-            del record["identifier"]
-            record[RhizomeField.ID.value] = new_ids
-            record[RhizomeField.URL.value] = new_urls
+            record["identifier"] = new_ids
+            record["url"] = new_urls
 
             # Split 'coverage' into values dealing with geography and values dealing with history (dates).
             coverage_values = record.get("coverage", [])
             if coverage_values:
 
-                new_hist_vals = []
-                new_geo_vals = []
+                hist_vals = []
+                geo_vals = []
 
                 for value in coverage_values:
 
                     if has_number(value=value):
 
-                        new_hist_vals.append(value)
+                        hist_vals.append(value)
 
                     else:
 
-                        new_geo_vals.append(value)
+                        geo_vals.append(value)
 
-                del record["coverage"]
-                record[RhizomeField.SUBJECTS_HISTORICAL_ERA.value] = new_hist_vals
-                record[RhizomeField.SUBJECTS_GEOGRAPHIC.value] = new_geo_vals
+                record["subjects_hist"] = hist_vals
+                record["subjects_geo"] = geo_vals
 
-
+        # Let base class do rest of transform.
         super().transform(data=data)
 
 
-if __name__ == "__main__":
+if __name__ == "__main__":    # pragma: no cover
 
     etl_process = PTHETLProcess(format="csv")
 
