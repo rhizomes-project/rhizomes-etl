5855395f77a230175d1d53c5e8ce12a9dddeba38
Karl Kaebnick <karl.kaebnick@gmail.com>
2021-01-19 18:55:42 -0500

    etl: notes about coverage

--- run_coverage.sh
+++ run_coverage.sh
@@ -12,6 +12,8 @@ export RUNNING_UNITTESTS=1
 
 pattern="*"
 # pattern="si"
+# pattern="pth"
+# pattern="dpla"
 
 coverage run -m unittest etl/tests/test*${pattern}*.py
 
@@ -25,3 +27,7 @@ then
     open htmlcov/index.html
 
 fi
+
+
+# REVIEW TODO collect statistics about each institutions - e.g., how much of each institution has
+# dates available and what different date formats are present for each?
