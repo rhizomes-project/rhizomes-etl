46b03aae41f7b9a91acb0c36769d4bddd1850083
Karl Kaebnick <karl.kaebnick@gmail.com>
2021-03-14 17:58:17 -0400

    etl: tests: add new testing features to better test pth's new data pull config

--- etl/tests/test_tools.py
+++ etl/tests/test_tools.py
@@ -15,8 +15,6 @@ import pdb
 
 
 REAL_GET = requests.get
-current_institution = None
-current_tag = None
 
 
 class MockResponse():
@@ -40,16 +38,18 @@ class MockGetter():
 
     def __call__(self, url, params=None, **kwargs):
 
+        test_info = ETLEnv.instance().get_test_info()
+
         self.num_calls = 0 if self.num_calls is None else self.num_calls + 1
 
-        for format in [ "json", "xml" ]:
+        for format_ in [ "json", "xml" ]:
 
-            data_path = f"etl/tests/data/{current_institution}"
-            if current_tag:
+            data_path = f"etl/tests/data/{test_info.institution}"
+            if test_info.tag:
 
-                data_path += f"_{current_tag}"
+                data_path += f"_{test_info.tag}"
 
-            data_paths = [ data_path + f".{format}", data_path + f"_{self.num_calls}.{format}" ]
+            data_paths = [ data_path + f".{format_}", data_path + f"_{self.num_calls}.{format_}" ]
 
             for data_path in data_paths:
 
@@ -57,7 +57,7 @@ class MockGetter():
 
                     with open(data_path, "r") as input:
 
-                        if format == "json":
+                        if format_ == "json":
 
                             return MockResponse(data=json.loads(input.read()))
 
@@ -66,7 +66,17 @@ class MockGetter():
                             return MockResponse(content=input.read())
 
         # No test data exists for this institutions: Just do the actual http get.
-        return REAL_GET(url=url, params=params, **kwargs)
+        data = REAL_GET(url=url, params=params, **kwargs)
+
+        if True:
+
+            with open(data_paths[0], "w") as output:
+
+                output.write(data.text)
+
+            print(f"Wrote data to test data file '{data_paths[0]}'", file=sys.stderr)
+
+        return data
 
 
 class TestBase(unittest.TestCase):
@@ -79,20 +89,12 @@ class TestBase(unittest.TestCase):
 
         ETLEnv.instance().init_testing()
 
-    def run_etl_test(self, institution, format, expected, tag=None):
+    def run_etl_test(self, institution, format, expected, tag=''):
 
-        if tag:
+        print(f"\nTesting ETL process for {institution} {tag}, format={format}\n", file=sys.stderr)
 
-            print(f"\nTesting ETL process for {institution} {tag}, format={format}\n", file=sys.stderr)
-
-        else:
-
-            print(f"\nTesting ETL process for {institution}, format={format}\n", file=sys.stderr)
-
-        global current_institution
-        current_institution = institution
-        global current_tag
-        current_tag = tag
+        test_info = ETLEnv.TestInfo(institution=institution, tag=tag)
+        ETLEnv.instance().set_test_info(test_info=test_info)
 
         if not self.debug:
 
