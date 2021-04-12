27ee6341c05d8fd2c2290d848343253102586591
Karl Kaebnick <karl.kaebnick@gmail.com>
2021-04-12 17:06:41 -0400

    etl: si: script to add image urls to SI csv in batches

---
+++ add_si_image_urls.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+import csv
+import sys
+
+
+from etl.etl_si import get_image_urls
+
+
+fieldnames = [ 'Resource Identifier', 'Title', 'Author/Artist', 'Description', 'Date', 'ResourceType', 'Digital Format', 'Dimensions', 'URL', 'Source', 'Language', 'Subjects (Historic Era)', 'Subjects (Topic/Keywords)', 'Subjects (Geographic)', 'Notes', 'Copyright Status', 'Collection Information', 'Credit Line', 'Images' ]
+
+
+if __name__ == "__main__":
+
+    input_file = sys.argv[1]
+
+    with open(input_file, 'r') as csvfile:
+
+        datareader = csv.reader(csvfile)
+        datawriter = csv.DictWriter(sys.stdout, fieldnames=fieldnames)
+        writer.writeheader()
+
+        first_row = True
+
+        for row in datareader:
+
+            if first_row:
+
+                first_row = False
+                continue
+
+            id_ = row[0]
+
+            if not id_:
+
+                pass
+
+            urls = get_image_urls(id_=id_)
+
+            if urls:
+
+                urls = '|'.join(urls)
+                row.append(urls)
+
+            writer.writerow(row)
