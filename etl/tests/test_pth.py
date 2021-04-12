dc0fed4c189645095caf1532fa5592e192066329
Karl Kaebnick <karl.kaebnick@gmail.com>
2021-04-12 12:42:01 -0400

    etl: pth: add creator test

--- etl/tests/test_pth.py
+++ etl/tests/test_pth.py
@@ -45,6 +45,12 @@ class TestPTH(TestBase):
 
         self.run_etl_test(institution="pth", tag="partner_dma", format="csv", expected=expected)
 
+    def test_creator_search(self):
+
+        expected = 'Resource Identifier,Title,Author/Artist,Description,Date,ResourceType,Digital Format,Dimensions,URL,Source,Language,Subjects (Historic Era),Subjects (Topic/Keywords),Subjects (Geographic),Notes,Copyright Status,Collection Information,Credit Line,Images\r\nark: ark:/67531/metapth836724 | info:ark/67531/metapth836724 | local-cont-no: Orendain_Antonio-2015-06-22,"Oral History Interview with Antonio Orendain, June 22, 2015","Jimenez, Luis | Orendain, Antonio","Interview with Antonio Orendain, civil rights activist and founder of the Texas Farm Workers Union (TFWU), from McAllen, Texas.  In the interview, Orendain discusses his childhood and family background, working with Cesar Chavez and Dolores Huerta and the National Farm Workers Association in California, migrant farm workers, founding the TFWU, and his long career as a labor activist.",2015-06-22,Video,,"3 videos : sd., col. ; digital | Video",https://texashistory.unt.edu/ark:/67531/metapth836724/,https://crbb.tcu.edu/interviews/interview-with-antonio-orendain,Spanish,,"Orendain, Antonio | People - Ethnic Groups | People - Individuals | biographies | interviews | oral histories",United States - Texas - Hidalgo County - McAllen,,,,,https://texashistory.unt.edu/ark:/67531/metapth836724/thumbnail\r\n\n'
+
+        self.run_etl_test(institution="pth", tag="creator_search", format="csv", expected=expected)
+
 
 if __name__ == '__main__':    # pragma: no cover
 
