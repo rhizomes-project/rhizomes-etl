568f98228f8c76c2efc3b19c4f374bdf6e61324f
Karl Kaebnick <karl.kaebnick@gmail.com>
2021-05-28 02:37:46 -0600

    etl: pth: handle yet more weird PTH date formats for searchable date

--- etl/etl_process.py
+++ etl/etl_process.py
@@ -3,6 +3,7 @@
 import abc
 import re
 import os
+import sys
 
 from etl.setup import ETLEnv
 from etl.tools import MetadataWriter, RhizomeField
@@ -38,27 +39,27 @@ def get_date_fourth_four(date_val):
 
     return get_date_four_chars(date_val=date_val, offset=3)
 
-def get_pth_year_range(date_val):
+def get_pth_year_range(date_val, offset=1):
 
     yr1 = int(date_val[1 : 5])
-    yr2 = int(date_val[7 : 11])
+    yr2 = int(date_val[5 + offset : 9 + offset])
 
     return int(((yr2 + yr1) / 2) + .5)
 
+def get_pth_year_range_skip2(date_val):
+
+    return get_pth_year_range(date_val=date_val, offset=2)
+
+def get_pth_year_range_offset6(date_val):
+
+    return get_pth_year_range(date_val=date_val, offset=6)
+
 def get_pth_date_range(date_val, offset=1):
 
-    try:
+    yr1 = int(date_val[1 : 5])
+    yr2 = int(date_val[11 + offset : 15 + offset])
 
-        yr1 = int(date_val[1 : 5])
-        yr2 = int(date_val[11 + offset : 15 + offset])
-
-        return int(((yr2 + yr1) / 2) + .5)
-
-    except:
-
-        # REVIEW: This should not be necessary.
-
-        pdb.set_trace()
+    return int(((yr2 + yr1) / 2) + .5)
 
 def get_pth_date_range_2del(date_val):
 
@@ -69,6 +70,10 @@ def get_pth_decade(date_val):
     yr = int(date_val.replace('X', '0'))
     return yr + 5
 
+def get_pth_decade_estimate(date_val):
+
+    return date_val[ : 3] + '5'
+
 
 
 DATE_PARSERS = {
@@ -78,27 +83,36 @@ DATE_PARSERS = {
     r'^\[\d{4}\.\.\]':                                      get_date_second_four,       # [1920..]
     r'^\{\d{4}\-\d{2}\-\d{2}':                              get_date_second_four,       # '{1843-10-01,1843-10-20}']
     r'^\[\d{4}\,\d{4}\]':                                   get_date_second_four,       # '[1930,1932]'
-    r'^\[\d{4}\.\.\d{4}\]$':                                get_pth_year_range,         # [1992..1998]
+    r'^\[\d{4}\.\.\d{4}\]$':                                get_pth_year_range_skip2,   # [1992..1998]
     r'^\[\d{4}\-\d{2}\-\d{2}\.\.\d{4}\-\d{2}\-\d{2}\]$':    get_pth_date_range_2del,    # [1900-01-22..1900-01-24]
-
     r'^\[\d{4}\-\d{2}\-\d{2}\,\d{4}\-\d{2}\-\d{2}\]$':      get_pth_date_range,         # [1900-01-22,1900-01-24]
-
     r'^\d{3}X':                                             get_pth_decade,             # 192X
-
     r'^\.\.\d{4}':                                          get_date_third_four,        # ..1840
     r'^\[\.\.\d{4}\]':                                      get_date_fourth_four,       # [..1840]
     r'^\d{4}\-\d{2}\-\d{2}\.\.':                            get_date_first_four,        # 1840-01-28..
-    r'^\[\d{4}\-\d{2}\-\d{2}\.\.\]':                        get_date_second_four,        # [1840-01-28..]
+    r'^\[\d{4}\-\d{2}\-\d{2}\.\.\]':                        get_date_second_four,       # [1840-01-28..]
+
+    r'^\d{3}[u\?\~]':                                       get_pth_decade_estimate,    # '188u' , '192?' , '196~'
+    # r'^\d{3}\?':                                            get_pth_decade_estimate,    # '192?'
+    # '196~'
+
+
+    r'^\{\d{4}\,\d{4}\}':                                   get_pth_year_range,         # {1930,1949}
+
+    r'^\{\d{4}\,\d{4}\,\d{4}\}':                            get_pth_year_range_offset6, # {1947,1956,1966}
+
+    # Final catch-all: try to extract 1st year.
+    r'^[\{\[]\d{4}':                                            get_date_second_four,       # {1936,1958~,1961,1962}
+
+    # Other PTH date vals we do not handle (yet?):
+    #
+    # 'unknown/1896'
+    # '19uu'
 
 }
 
 def get_searchable_date(record):
     "Parse the date val and extract a year from it."
-
-
-    import pdb
-    # pdb.set_trace()
-
 
     date_vals = record.get(RhizomeField.DATE.value, [])
     if type(date_vals) is not list:
@@ -128,11 +142,14 @@ def get_searchable_date(record):
                     return searchable_date
 
 
-    # REVIEW: remove this
+    # REVIEW: remove this?
     if date_vals:
 
 
         pdb.set_trace()
+
+
+        print(f"Ignoring date val '{date_vals[0]}' - could not parse searchable date", file=sys.stderr)
 
 
 def clean_value(value):
